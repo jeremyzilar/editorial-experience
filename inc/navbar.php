16430763eca6390fd5d5f8cdc32fc2df5c76a10c
Jeremy Zilar <jeremyzilar@gmail.com>
2014-04-11 12:30:02 -0400

    adding in dynamic navigation

---
+++ inc/navbar.php
@@ -0,0 +1,7 @@
+<div class="navbar">
+  <ul class="nav nav-pills pull-left">
+    <li class="active"><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+</div>
