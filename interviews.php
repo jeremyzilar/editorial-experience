0fd87aa769e6cf84c641c2fb90b8a4c62307eef5
Jeremy Zilar <jeremyzilar@gmail.com>
2014-05-04 22:54:34 -0400

    adding checks to interview logic

--- interviews.php
+++ interviews.php
@@ -42,7 +42,7 @@
               }
 
               if (has_post_thumbnail( $post->ID ) ) {
-                $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
                 $image = $image[0];
               } else {
                 $image = TDIR . '/img/bear.jpg';
