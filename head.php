1b2ae42eee0c2361806ef8711db8979e792c732a
Jeremy Zilar <jeremyzilar@gmail.com>
2014-05-08 22:23:26 -0400

    edits

--- head.php
+++ head.php
@@ -9,7 +9,7 @@
           <div class="col-sm-8 col-md-9">
             <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
             <p class="lead"><?php bloginfo( 'description' ); ?></p>
-            <p class="blurb">Sue Apfelbaum and Juliette Cezzar dive deep into the contemporary world of editorial design to share time-tested principles; 26 interviews with experts in art direction, user experience, content strategy, and product design; and 19 case studies on the publications who are creating the new media landscape.</p>
+            <p class="blurb">Sue Apfelbaum and Juliette Cezzar dive deep into the contemporary world of editorial design to share time-tested principles; 26 interviews with experts in art direction, user experience, content strategy, and product design; and 19 case studies on the publications who are pioneering the new media landscape.</p>
             <p><a class="btn btn-sm btn-learn" data-id="#blog" href="<?php echo esc_url( home_url( '/' ) ); ?>" role="button">Learn More</a> <a class="btn btn-sm btn-buy" href="http://www.amazon.com/dp/1592538959/?tag=themapstrsch-20" role="button">Buy the Book</a></p>
           </div>
         </div>
