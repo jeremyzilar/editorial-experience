267e7c3ba1a9577a24e0621c5d78353a77c62543
Jeremy Zilar <jeremyzilar@gmail.com>
2014-05-08 23:49:10 -0400

    fixes of all kinds

--- single.php
+++ single.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
 
-<div id="blog">
-	<div class="row">
-
-		<div class="<?php echo GRID; ?>">
-			<div class="blog-head">
-				<h1>Designing the Editorial Experience</h1>
-				<h3>Observations and News from the wild</h3>
-				<div class="divider"></div>
+<section id="blog">
+	<div class="container">
+		<div class="row">
+			<div id="entry-box" class="<?php echo GRID; ?>">
+				<div class="blog-head">
+					<h1>Designing the Editorial Experience</h1>
+					<h3>News &amp; Insights on Editorial Design Across Media</h3>
+					<div class="divider"></div>
+				</div>
+				<?php loop(); ?>
 			</div>
-			<?php loop(); ?>
+			<?php get_sidebar(); ?>
 		</div>
-		<?php get_sidebar(); ?>
 	</div>
-</div>
+</section>
 
 <?php get_footer(); ?>
