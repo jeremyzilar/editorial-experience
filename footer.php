f69ceb5583049064f3b4c11c1881dda96c541bce
Jeremy Zilar <jeremyzilar@gmail.com>
2014-05-12 17:14:50 -0400

    updaing image functions

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-27200315-2', 'editorialexperience.com');
+  ga('require', 'displayfeatures');
   ga('send', 'pageview');
 
 </script>
